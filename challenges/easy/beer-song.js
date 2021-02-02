dec3e6898a34f54d351bf0e3ba09999cae2a5a39
Caleb Smith <smithcaleb042@gmail.com>
2021-02-02 15:40:52 -0700

    completed a few challenges

---
+++ challenges/easy/beer-song.js
@@ -0,0 +1,33 @@
+/*
+Problem:
+input: verse number
+output: the string verse of the beer song
+rules:
+1st verse = 99 bottles of beer
+99th verse = 0 bottles of beer
+
+
+Examples: SEE TESTS
+
+
+Data Structures:
+string outputs
+number inputs
+class
+no constructor
+
+Algorithm:
+
+
+//verse 1 should start with 99 bottle of beer on the wall
+verse 99 should be no more bottles of beer
+the last verse is when you get more beer
+
+*/
+class BeerSong {
+  static verse(verse) {
+    
+  }
+}
+
+module.exports = BeerSong;
