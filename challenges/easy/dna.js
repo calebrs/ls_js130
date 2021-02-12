a6d21c69e5ca5e8027ed67b77d16cf90628b88cb
Caleb Smith <smithcaleb042@gmail.com>
2021-02-12 16:30:42 -0700

    more test prep

--- challenges/easy/dna.js
+++ challenges/easy/dna.js
@@ -1,56 +1,58 @@
 /*
 Problem:
-input: two strings 
-output: a number, representing the number of dirernces between the two strings
+calculate hamming distance. The difference between two input DNA strands
+inputs: two strings that represent DNA
 
-
-Examples: See testing code
+Examples: SEE TEST CODE
 
 
 Data Structures:
-A class that contains two methods and one property:
-constructor:
-    declares strand property
+DNA class
 
-hammingDistance:
-    calculates the number of differences between the strand string and the
-    input sting
+constructor that takes one argument 
+
+hammingDistance MEthod takes one string argument
 
 
 Algorithm:
-FOR hammindDistance methdod:
-    takes one string input:
-        delcare a counter
-        loop through the this.strand string
-            on each char, compare the current char to the current char of the
-            input string
-            if they are different add one to a counter
-            return the count
+constructor:
+  assign this.DNA to the input DNA strand
+
+hammingDistance method:
+  declare result variable
+  define shorterDNA by sorting the two strands by length an taking the first one
+  define loneDNA by taking the longer one
+  begin iterating through the shorter DNA
+    on each iteration compare the current char to the current char in the longer strand
+    if the chars are different, add one to the result
+
+  return the result
 
 
 */
-
 class DNA {
-    constructor(strand) {
-        this.strand = strand;
+  constructor(strand) {
+    this.strand = strand;
+  }
+
+  hammingDistance(inStrand) {
+    let result = 0;
+    let strands = [inStrand, this.strand]
+                  .sort((a, b) => a.length - b.length);
+    let longerStrand = strands[1];
+    let shorterStrand = strands[0];
+    
+    for (let indx = 0; indx < shorterStrand.length; indx += 1) {
+      let currentChar = shorterStrand[indx];
+      let compareChar = longerStrand[indx];
+      if (currentChar !== compareChar) {
+        result += 1;
+      }
     }
-
-    hammingDistance(distance) {
-        let total = 0;
-
-        for (let indx = 0; indx < this.strand.length; indx += 1) {
-            let currentStrand = this.strand[indx];
-            let currentDistance = distance[indx];
-
-            if (!currentDistance) {
-                continue;
-            } else if (currentDistance !== currentStrand) {
-                total += 1;
-            }
-        }
-
-        return total;
-    }
+    
+    return result;
+  }
 }
-
+// let test = new DNA('GGACTGA');
+// console.log(test.hammingDistance('GGACTGA'));
 module.exports = DNA;
